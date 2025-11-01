8f6c94fcfcab201a93cde1c7bbf22b0041d5ddb9
KristofferDe <kndelosr@csu.fullerton.edu>
2025-10-31 17:40:44 -0700

    F
    F

--- Assets/Script/PlayerMovment.cs
+++ Assets/Script/PlayerMovment.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerMovment : MonoBehaviour
 {
     public float movmentSpeed = 7f;
+    public Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +14,8 @@ public class PlayerMovment : MonoBehaviour
     // Update is called once per frame
     void  FixedUpdate()//This makes it run by game time
     {
-        new Vector3 moveDir = transform.right*Input.GetAxis("Horizontal") * movmentSpeed + transform.forward*Input.GetAxis("Vertical")*movmentSpeed;
+        Vector3 moveDir = transform.right*Input.GetAxis("Horizontal") * movmentSpeed + transform.forward*Input.GetAxis("Vertical")*movmentSpeed;
+        
         //This vector repesents where we want to go, our new direction
         
     }
